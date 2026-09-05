b824c324d4da2d7c0a914e13f4852860a2e5237f
Victor Widell <victor.widell@divid.se>
2026-09-05 20:54:16 +0200

    Added charset.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,41 @@
+#include <c64/cia.h>
+#include <c64/memmap.h>
+#include <c64/vic.h>
+#include <stdint.h>
 #include <stdio.h>
+
+// Config the regions and sections.
+
+// Create a "lower" region before the charset.
+#pragma region( lower, 0x0a00, 0x2000, , , {code, data} )
+// Create a "charset" section.
+#pragma section(charset, 0)
+// Create a "charset" region to contain the "charset" section.
+// Note: The charset and screen must be in the same 16k vic bank.
+#pragma region( charset, 0x2000, 0x2800, , , {charset} )
+// Adjust the "main" region to start after the "charset" region.
+#pragma region( main, 0x2800, 0xa000, , , {code, data, bss, heap, stack} )
+
+// Store the charset in the "charset" section.
+#pragma data(charset)
+char charset[2048] = {
+#include "worm-charset.inc"
+};
+// End "charset" section.
+#pragma data(data)
+
+void setup(void)
+{
+	// Clear interrupts set by basic/kernal, or it will crash when it is unmapped
+	cia_init();
+	// Unmap basic/kernal/rom-charsets.
+	mmap_set(MMAP_NO_ROM);
+	vic_setmode(VICM_TEXT, (uint8_t *)0x0400, charset);
+}
 
 int main()
 {
-	printf("HELLO WORLD");
+	setup();
+
+	return 0;
 }
