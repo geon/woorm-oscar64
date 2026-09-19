2abbe73f277f758b40f44306fd175542f47fb8fd
Victor Widell <victor.widell@divid.se>
2026-09-19 11:44:12 +0200

    Made the screen small for tests.

--- src/screen.h
+++ src/screen.h
@@ -3,8 +3,14 @@
 
 #include <stdint.h>
 
+#ifdef TEST
+#define SCREEN_WIDTH 8
+#define SCREEN_HEIGHT 8
+#else
 #define SCREEN_WIDTH 40
 #define SCREEN_HEIGHT 25
+#endif
+
 #define SCREEN_SIZE (SCREEN_WIDTH * SCREEN_HEIGHT)
 
 extern uint8_t *const screenChars;
